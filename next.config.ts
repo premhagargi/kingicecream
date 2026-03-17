12bad815f3e072a05383629797370dd83ea573ae
Prem Hagargi <prem@bloortech.com>
2026-03-17 15:22:25 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://luccian

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'luccianos.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
